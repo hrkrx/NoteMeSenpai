02c1c68d4838410325db79557a802bc46ef245a1
hrkrx <5176531+hrkrx@users.noreply.github.com>
2020-06-26 16:15:13 +0200

    pull instead clone ...

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,8 +2,7 @@ mongod&
 sleep 2
 while :
 do
-git clone --recurse-submodules https://github.com/hrkrx/NoteMeSenpai
-cd /NoteMeSenpai/NoteMeSenpai && dotnet publish -o /build/
+cd /NoteMeSenpai/NoteMeSenpai && git pull && dotnet publish -o /build/
 cd /build
 dotnet NoteMeSenpai.dll
 done
